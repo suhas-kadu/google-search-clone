e044e7d8d34775ce05390b619efb063c6bfea9d0
suhas-kadu <kadusuhas72@gmail.com>
2022-04-29 02:39:00 +0530

    fix:purge replaced by content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: "class", // or 'media' or 'class'
-  content: [],
+
   theme: {
     extend: {},
   },
